ac68a4816749d0c2bb801b6418052c404af8a1e8
SergStas <sergstas0@gmail.com>
2021-12-07 11:48:55 +0500

    More sample data

--- data/data_sample.sql
+++ data/data_sample.sql
@@ -1,26 +1,59 @@
-insert into user values
-(
-    0, "@dummy", "test_user"
-);
+insert into user values(-1, "dummy", "test_user#1");
+insert into user values(-2, "dummy", "test_user#2");
+insert into user values(-3, "dummy", "test_user#3");
+insert into user values(-4, "dummy", "test_user#4");
+insert into user values(-5, "dummy", "test_user#5");
 
-insert into character values(1, "arcturus", "human", 1, "test class", "this is the test character#1", "test heaven value", "https://google.com");
-insert into character values(2, "arcturus", "human", 2, "test class", "this is the test character#2", "test heaven value", "https://google.com");
-insert into character values(3, "arcturus", "human", 3, "test class", "this is the test character#3", "test heaven value", "https://google.com");
-insert into character values(4, "arcturus", "human", 4, "test class", "this is the test character#4", "test heaven value", "https://google.com");
-insert into character values(5, "arcturus", "human", 5, "test class", "this is the test character#5", "test heaven value", "https://google.com");
-insert into character values(6, "arcturus", "human", 6, "test class", "this is the test character#6", "test heaven value", "https://google.com");
-insert into character values(7, "arcturus", "human", 7, "test class", "this is the test character#7", "test heaven value", "https://google.com");
-insert into character values(8, "arcturus", "human", 8, "test class", "this is the test character#8", "test heaven value", "https://google.com");
-insert into character values(9, "arcturus", "human", 9, "test class", "this is the test character#9", "test heaven value", "https://google.com");
-insert into character values(10, "arcturus", "human", 10, "test class", "this is the test character#10", "test heaven value", "https://google.com");
 
-insert into lot values (1, 1, 0, 977702400, null , 10000, "fake@contact.nill");
-insert into lot values (2, 2, 0, 977702400, null , 20000, "fake@contact.nill");
-insert into lot values (3, 3, 0, 977702400, null , 30000, "fake@contact.nill");
-insert into lot values (4, 4, 0, 977702400, null , 40000, "fake@contact.nill");
-insert into lot values (5, 5, 0, 977702400, null , 50000, "fake@contact.nill");
-insert into lot values (6, 6, 0, 977702400, null , 60000, "fake@contact.nill");
-insert into lot values (7, 7, 0, 977702400, null , 70000, "fake@contact.nill");
-insert into lot values (8, 8, 0, 977702400, null , 80000, "fake@contact.nill");
-insert into lot values (9, 9, 0, 977702400, null , 90000, "fake@contact.nill");
-insert into lot values (10, 10, 0, 977702400, null , 100000, "fake@contact.nill");
+
+insert into character values(1, "arcturus", "human", 1, "test class", "this is the test character #1", "test heaven value", "https://google.com");
+insert into character values(2, "arcturus", "human", 2, "test class", "this is the test character #2", "test heaven value", "https://google.com");
+insert into character values(3, "arcturus", "human", 3, "test class", "this is the test character #3", "test heaven value", "https://google.com");
+insert into character values(4, "arcturus", "human", 4, "test class", "this is the test character #4", "test heaven value", "https://google.com");
+insert into character values(5, "arcturus", "human", 5, "test class", "this is the test character #5", "test heaven value", "https://google.com");
+insert into character values(6, "arcturus", "human", 6, "test class", "this is the test character #6", "test heaven value", "https://google.com");
+insert into character values(7, "arcturus", "human", 7, "test class", "this is the test character #7", "test heaven value", "https://google.com");
+insert into character values(8, "arcturus", "human", 8, "test class", "this is the test character #8", "test heaven value", "https://google.com");
+insert into character values(9, "arcturus", "human", 9, "test class", "this is the test character #9", "test heaven value", "https://google.com");
+insert into character values(10, "arcturus", "human", 10, "test class", "this is the test character #10", "test heaven value", "https://google.com");
+
+insert into character values(11, "arcturus", "human", 1, "test class", "this is the test character #11", "test heaven value", "https://google.com");
+insert into character values(12, "arcturus", "human", 2, "test class", "this is the test character #12", "test heaven value", "https://google.com");
+insert into character values(13, "arcturus", "human", 3, "test class", "this is the test character #13", "test heaven value", "https://google.com");
+insert into character values(14, "arcturus", "human", 4, "test class", "this is the test character #14", "test heaven value", "https://google.com");
+insert into character values(15, "arcturus", "human", 5, "test class", "this is the test character #15", "test heaven value", "https://google.com");
+
+insert into character values(16, "hyperion", "human", 6, "test class", "this is the test character #16", "test heaven value", "https://google.com");
+insert into character values(17, "hyperion", "untamed", 7, "test class", "this is the test character #17", "test heaven value", "https://google.com");
+insert into character values(18, "hyperion", "untamed", 8, "test class", "this is the test character #18", "test heaven value", "https://google.com");
+
+insert into character values(19, "hyperion", "untamed", 9, "test class", "this is the test character #19", "test heaven value", "https://google.com");
+
+insert into character values(20, "hyperion", "untamed", 10, "test class", "this is the test character #20", "test heaven value", "https://google.com");
+
+
+
+insert into lot values (1, 1, -1, 977702400, null , 10000, "fake@contact.nill");
+insert into lot values (2, 2, -1, 977702400, null , 20000, "fake@contact.nill");
+insert into lot values (3, 3, -1, 977702400, null , 30000, "fake@contact.nill");
+insert into lot values (4, 4, -1, 977702400, null , 40000, "fake@contact.nill");
+insert into lot values (5, 5, -1, 977702400, null , 50000, "fake@contact.nill");
+insert into lot values (6, 6, -1, 977702400, null , 60000, "fake@contact.nill");
+insert into lot values (7, 7, -1, 977702400, null , 70000, "fake@contact.nill");
+insert into lot values (8, 8, -1, 977702400, null , 80000, "fake@contact.nill");
+insert into lot values (9, 9, -1, 977702400, null , 90000, "fake@contact.nill");
+insert into lot values (10, 10, -1, 977702400, null , 100000, "fake@contact.nill");
+
+insert into lot values (11, 11, -2, 977702400, null , 110000, "fake@contact.nill");
+insert into lot values (12, 12, -2, 977702400, null , 120000, "fake@contact.nill");
+insert into lot values (13, 13, -2, 977702400, null , 130000, "fake@contact.nill");
+insert into lot values (14, 14, -2, 977702400, null , 140000, "fake@contact.nill");
+insert into lot values (15, 15, -2, 977702400, null , 150000, "fake@contact.nill");
+
+insert into lot values (16, 16, -3, 977702400, null , 160000, "fake@contact.nill");
+insert into lot values (17, 17, -3, 977702400, null , 170000, "fake@contact.nill");
+insert into lot values (18, 18, -3, 977702400, null , 180000, "fake@contact.nill");
+
+insert into lot values (19, 19, -4, 977702400, null , 190000, "fake@contact.nill");
+
+insert into lot values (20, 20, -5, 977702400, null , 200000, "fake@contact.nill");
